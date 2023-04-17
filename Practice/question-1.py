4fdfe4199d1270226cf52fd2b159c64df0954500
Shree <109030986+Shree-77@users.noreply.github.com>
2023-04-17 20:03:09 +0530

    Question1 and 2 updated

--- Practice/question-1.py
+++ Practice/question-1.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Apr 17 18:58:01 2023
-
-@author: navee
+1.	Write a python program to that accepts length of three sides of a triangle as inputs. The program should indicate whether or not the triangle is a right-angled triangle.
+[Hint: Using Pythagorean theorem]
 """
+import math 
 
+a=float(input("Enter the length of 1st side: "))
+b=float(input("Enter the length of 2nd side : "))
+
+c=math.sqrt(a**2+b**2)
+
+print(c)
---
+++ Practice/question-2.py
@@ -0,0 +1,24 @@
+"""
+2.	Write a python program that helps in updating and displaying the user’s bank balance based on their type of transactions. 
+"""
+balance = float(input("Enter the account balance :  "))
+
+
+operations = int(input(
+    "Enter which operation you want to perform : \n1.Deposit \n 2.Withdraw \n 3.view balance \n 4.exit \n" ))
+
+if operations == 1:
+    deposit=float(input("Enter the amount to be deposited : "))
+    balance= balance+deposit
+    print(" Deposit Successful!! /n Your current balance is " , balance)
+elif operations==2:
+    if balance<0:
+        print("Insufficient balance")
+    else : 
+        Withdraw = float(input("Enter the amount to be withdraw : "))    
+        balance-=Withdraw
+        print(" Withdraw Successful!! /n Your current balance is " , balance)
+elif operations==3:
+    print("Your current balance is : " , balance)
+else : 
+    print("Enter the correct number")        
